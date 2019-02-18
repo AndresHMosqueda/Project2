51253e9fe8c29d72db5084bbbe1250fa5bf3a611
Andres <andreshm94@gmail.com>
2019-02-17 18:38:54 -0600

    Inprogress

---
+++ models/ListCar.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+  carmodel: String,
+  owner: String,
+  features: String,
+  imagecar: String,
+  description: String,
+  startDate: String,
+  endDate: String,
+  status: {
+    type: String,
+    enum: ['Available', 'Not Available'],
+    default: 'Available'
+  },
+  priceperday: Number
+},
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+const User = mongoose.model('Car', carSchema);
+module.exports = User;
